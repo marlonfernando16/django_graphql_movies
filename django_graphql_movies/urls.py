e845b66ba589d618193ae1890b2f6b856797ed07
marlonfernando16 <marlon.fernando16@outlook.com>
2019-12-09 13:30:48 -0300

    Inserindo csrf para requisições externas para a API

--- django_graphql_movies/urls.py
+++ django_graphql_movies/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django_graphql_movies.schema import schema
+from django.views.decorators.csrf import csrf_exempt # New library
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
